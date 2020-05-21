4adc69434c96a6e946467763af72b42f512f8b26
rajanchettri <rajan_chettri@live.com>
2020-05-21 10:27:11 +0530

    1

---
+++ Story.java
@@ -0,0 +1,10 @@
+class Story{
+
+	public static void main(String[] args){
+
+	System.out.println("i am the earth");
+	System.out.println("i am the four winds blowing");
+	System.out.println("i am the earth");
+}
+
+}
